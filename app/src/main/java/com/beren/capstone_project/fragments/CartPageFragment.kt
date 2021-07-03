6bef5e2746029296239ede2f113a1911e46bdaa0
berenkotanli <berenkotanli@gmail.com>
2021-07-03 10:43:58 +0300

    cart page update

--- app/src/main/java/com/beren/capstone_project/fragments/CartPageFragment.kt
+++ app/src/main/java/com/beren/capstone_project/fragments/CartPageFragment.kt
@@ -1,5 +1,6 @@
 package com.beren.capstone_project.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,20 +11,20 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.beren.capstone_project.R
+import com.beren.capstone_project.SwipeGesture
 import com.beren.capstone_project.adapters.CartItemsAdapter
-import com.beren.capstone_project.adapters.ProductsAdapter
 import com.beren.capstone_project.databinding.FragmentCartPageBinding
 import com.beren.capstone_project.retrofit.ApiUtils
 import com.beren.capstone_project.retrofit.ProductsDaoInterface
 import com.beren.capstone_project.viewmodel.CartPageFragmentViewModel
-import com.beren.capstone_project.viewmodel.CourseDetailsFragmentViewModel
-import com.beren.capstone_project.viewmodel.HomePageFragmentViewModel
-import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_cart_page.*
 
 
-class CartPageFragment : Fragment() {
+class CartPageFragment() : Fragment() {
     private lateinit var design: FragmentCartPageBinding
     private lateinit var viewModel: CartPageFragmentViewModel
     private lateinit var pdaoi: ProductsDaoInterface
@@ -36,19 +37,37 @@ class CartPageFragment : Fragment() {
         pdaoi= ApiUtils.getProductsDaoInterface()
         design.cartPageFragment=this
 
+      //  design.emptyTxt
+      /*  val swipegesture= object :SwipeGesture(requireContext()){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
 
+                when(direction){
+                    ItemTouchHelper.LEFT ->{
+                        viewModel.deleteCartItem(21,0)
+                    } } } }
+        val touchHelper= ItemTouchHelper(swipegesture)
+        touchHelper.attachToRecyclerView(rv_cart_items)
+*/
         viewModel.cartItemsList.observe(viewLifecycleOwner, { cartItemsList ->
             adapter = CartItemsAdapter(requireContext(), cartItemsList,viewModel)
             design.cartadapter = adapter
         })
         design.rvCartItems.layoutManager = StaggeredGridLayoutManager(5, StaggeredGridLayoutManager.HORIZONTAL)
         design.rvCartItems.addItemDecoration(DividerItemDecoration(activity,StaggeredGridLayoutManager.HORIZONTAL))
+
+     /*  viewModel.cartItemsList.observe(viewLifecycleOwner,{
+           if (it.size==0){
+               design.emptyTxt.visibility=View.VISIBLE
+           }
+       })*/
         return design.root
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val temp: CartPageFragmentViewModel by viewModels()
         viewModel=temp
+
     }
 
     fun deleteCartItem(id:Int,cart_status:Int){
